5493a3b1e6d9da70230cb2318907e743fb61f422
František Vojáček <fr.vojacek@email.cz>
2023-12-16 14:42:26 +0100

    feat: add map parsing

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import * as fs from 'fs'
+import { URL } from 'url'
+
+const inputPath = new URL('../tests/test0.in', import.meta.url)
+const input = fs.readFileSync(inputPath).toString()
+
+const lines = input.split(/\r?\n/)
+const gridSize = lines[0].length
+const grid = lines.slice(0, gridSize).map(line => line.split(''))
+const scannedValues = lines.slice(gridSize).map(line => line.split(' ').map(x => Number(x)))
+const distances = getDistances()
+
+function getDistances() {
+  let distances = []
+  let scannedIndex = 0
+
+  grid.forEach((row, rowIndex) => {
+    row.forEach((cell, columnIndex) => {
+      if(cell === 'A') {
+        distances.push([rowIndex, columnIndex, ...scannedValues[scannedIndex]])
+        scannedIndex++
+      }
+    })
+  });
+
+  return distances
+}
